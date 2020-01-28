b87d78981a33220201bc1833dc023292a7bb447a
Philip Montagu-Evans <pmontaguevans@gmail.com>
2020-01-28 20:41:17 +0100

    Adjust for AWS beanstalk

--- app.ts
+++ app.ts
@@ -2,5 +2,5 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello world!'));
+app.get('/api', (req, res) => res.send('Hello world!'));
 app.listen('3001');
